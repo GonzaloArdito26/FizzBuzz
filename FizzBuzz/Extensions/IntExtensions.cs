826310324ff4a634af9070dd4a4828f7159f6156
gonzaloardito <gonzalo.ardito26@gmail.com>
2017-06-16 01:11:18 -0300

    Initial commit.

---
+++ FizzBuzz/Extensions/IntExtensions.cs
@@ -0,0 +1,11 @@
+﻿
+namespace FizzBuzz.Extensions
+{
+    public static class IntExtensions
+    {
+        public static bool MultipleOf(this int a, int b)
+        {
+            return (a % b) == 0;
+        }
+    }
+}
--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using FizzBuzz.Extensions;
 
 namespace FizzBuzz
 {
@@ -10,6 +7,32 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+
+            bool multipleOfThree, multipleOfFive;
+
+            for (var i = 1; i <= 100; i++)
+            {
+                multipleOfThree = i.MultipleOf(3);
+                multipleOfFive = i.MultipleOf(5);
+
+                if (multipleOfThree && multipleOfFive)
+                {
+                    Console.WriteLine("fizzbuzz");
+                }
+                else if (multipleOfThree)
+                {
+                    Console.WriteLine("fizz");
+                }
+                else if (multipleOfFive)
+                {
+                    Console.WriteLine("buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }                
+            }
+            Console.ReadLine();
         }
     }
 }
